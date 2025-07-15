bf903d80e8c8b4a34c5c73e390820e8043759500
Trancute123 <146714485+Trancute123@users.noreply.github.com>
2025-07-16 01:05:24 +0700

    push unfinished FilterPage with bugs to fix later

--- src/pages/FilterPage.jsx
+++ src/pages/FilterPage.jsx
@@ -3,15 +3,17 @@ import { useSelector } from "react-redux";
 import Header from "../components/layout/Header";
 import SearchBar from "../components/SearchBar";
 import CategoryFilter from "../components/CategoryFilter";
-import Gallery from "../components/Gallery";
 import images from "../data/images";
+import Gallery from "../components/Gallery";
 
 export default function FilterPage() {
   const { category, searchTerm } = useSelector((state) => state.filter);
 
   const filteredImages = images.filter((img) => {
     const matchCategory = category === "All" || img.category === category;
-    const matchSearch = img.name.toLowerCase().includes(searchTerm.toLowerCase());
+    const matchSearch = img.name
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
     return matchCategory && matchSearch;
   });
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
-import { configureStore } from '@reduxjs/toolkit';
-import filterReducer from './slices/filterSlice';
-import modalReducer from './slices/modalSlice';
-import favoriteReducer from './slices/favoriteSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import filterReducer from "./slices/filterSlice";
+import modalReducer from "./slices/modalSlice";
+import favoriteReducer from "./slices/favoriteSlice";
 
 const store = configureStore({
   reducer: {
