0f69c3e573acb53e0d2d909f84270d6f3df1591b
Tio-san92 <kientntde181056@fpt.edu.vn>
2025-07-16 00:02:04 +0700

    add filter

---
+++ src/pages/FilterPage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Header from "../components/layout/Header";
+import SearchBar from "../components/SearchBar";
+import CategoryFilter from "../components/CategoryFilter";
+import Gallery from "../components/Gallery";
+import images from "../data/images";
+
+export default function FilterPage() {
+  const { category, searchTerm } = useSelector((state) => state.filter);
+
+  const filteredImages = images.filter((img) => {
+    const matchCategory = category === "All" || img.category === category;
+    const matchSearch = img.name.toLowerCase().includes(searchTerm.toLowerCase());
+    return matchCategory && matchSearch;
+  });
+
+  return (
+    <>
+      <Header />
+      <SearchBar />
+      <CategoryFilter />
+      <Gallery images={filteredImages} />
+    </>
+  );
+}
