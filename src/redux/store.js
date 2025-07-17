5d12e8ae384ef92ffd968be97a3954ce2fbf9286
Vo Thi Thuy Duyen <vothuyduyen1414@gmail.com>
2025-07-17 23:14:19 +0700

    guest page

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 import filterReducer from "./slices/filterSlice";
 import modalReducer from "./slices/modalSlice";
 import favoriteReducer from "./slices/favoriteSlice";
+import authReducer from "./slices/authSlice";
 
 const store = configureStore({
   reducer: {
+    auth: authReducer,
     filter: filterReducer,
     modal: modalReducer, 
     favorite: favoriteReducer,
