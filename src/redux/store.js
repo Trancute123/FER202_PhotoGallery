4ef2e246516e9ace7fe3f2b3a681fc66b070bd71
Trancute123 <146714485+Trancute123@users.noreply.github.com>
2025-07-20 13:41:22 +0700

    updated dark/light for gallerypage

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import filterReducer from "./slices/filterSlice";
 import modalReducer from "./slices/modalSlice";
 import favoriteReducer from "./slices/favoriteSlice";
 import authReducer from "./slices/authSlice";
-import imageReducer from "./slices/imageSlice"; // Đảm bảo dòng này có
+import themeReducer from "./slices/themeSlice";
 
 const store = configureStore({
   reducer: {
@@ -11,7 +11,7 @@ const store = configureStore({
     filter: filterReducer,
     modal: modalReducer,
     favorite: favoriteReducer,
-    image: imageReducer, // Đảm bảo dòng này có
+    theme: themeReducer,
   },
 });
 
